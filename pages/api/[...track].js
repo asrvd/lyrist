9fb767666f1a10622661576d37515f68e1290b55
ashish <ashirvadbhushan@gmail.com>
2022-04-11 18:59:56 +0530

    FML

--- pages/api/[...track].js
+++ pages/api/[...track].js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     const { track } = req.query
     console.log(track)
     if (track.length === 2) {
-        await getLyrics(`${track[0]} ${track[1]}`).then(l => {
+        await getLyrics(`${decodeURIComponent(track[0])} ${decodeURIComponent(track[1])}`).then(l => {
             res.statusCode = 200
             res.setHeader("Content-Type", "application/json")
             // console.log(l)
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
             // console.log(err)
             res.setHeader("Content-Type", "application/json")
             res.end(JSON.stringify({ error: "Lyrics NotFound" }))
+            
         })
         
     } else {
