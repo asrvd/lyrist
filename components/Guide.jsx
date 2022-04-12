a0845c65a2beb52c758ef7ed8d640d13cd387c2c
ashish <ashirvadbhushan@gmail.com>
2022-04-12 08:27:03 +0530

    Minor Update

--- components/Guide.jsx
+++ components/Guide.jsx
@@ -2,7 +2,7 @@ import { CopyBlock, dracula, CodeBlock, ocean } from "react-code-blocks";
 import { Text } from "@chakra-ui/react";
 
 export default function Guide() {
-    const text = "await fetch('https://lyrist.vercel.app/api/fumes/eden').then(\n\tres => {\n\tif (res.status === 200) {\n\t\tconsole.log(res.json()\n\t} else {\n\t\tconsole.log('Not Found)\n\t}\n)";
+    const text = "await fetch('https://lyrist.vercel.app/api/fumes/eden').then(\n\tres => {\n\tif (res.status === 200) {\n\t\tconsole.log(res.json()\n\t} else {\n\t\tconsole.log('Not Found')\n\t}\n)";
     return (
         <div className="guide-container">
             <h2>API EndPoint</h2>
--- pages/api/[...track].js
+++ pages/api/[...track].js
@@ -2,7 +2,7 @@ import { getLyrics } from "lyrics-dumper"
 
 export default async function handler(req, res) {
     const { track } = req.query
-    console.log(track)
+    // console.log(track)
     if (track.length === 2) {
         await getLyrics(`${decodeURIComponent(track[0])} ${decodeURIComponent(track[1])}`).then(l => {
             res.statusCode = 200
@@ -13,11 +13,9 @@ export default async function handler(req, res) {
             res.statusCode = 404
             // console.log(err)
             res.setHeader("Content-Type", "application/json")
-            res.end(JSON.stringify({ error: "Lyrics NotFound" }))
-            
-        })
-        
+            res.end(JSON.stringify({ error: 'Lyrics NotFound' })) 
+        }) 
     } else {
-        res.status(400).end('Invalid Parameters, You need to provide 2 parameters: track name and artist name.')
+        res.status(400).json({error: 'Invalid Parameters, You need to provide 2 parameters: track name and artist name.'})
     }
 }
