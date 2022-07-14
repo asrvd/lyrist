c970d4eafb5367829a4fa209c2d58d861ffd42fd
ashish <68690233+asheeeshh@users.noreply.github.com>
2022-07-14 08:50:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,17 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Head from "next/head"
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>lyrist</title>
+        <meta name="description" content="lyrics api that just works." />
+        <script async defer data-website-id="29e9b38d-3fb5-4cf6-a120-1731741019e1" src="https://umami.asheeshh.ga/umami.js"></script>
+        <link rel="icon" href="https://favmoji.asheeshh.ga/🎧" />
+      </Head>
       <Header />
       <Lyrics />
       <Footer />
