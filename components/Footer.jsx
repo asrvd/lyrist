a090bf01790c589442a4e3152cb7c47c0fbf6eea
ashish <68690233+asrvd@users.noreply.github.com>
2022-07-21 15:03:09 +0530

    update username

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="footer-container">
             <div className="footer-body">
-                <p>made with {`<3`} by <a href="https://github.com/asheeeshh/">ashish</a> using <a href="https://nextjs.org/">next.js</a>.</p>
+                <p>made with {`<3`} by <a href="https://github.com/asrvd/">ashish</a> using <a href="https://nextjs.org/">next.js</a>.</p>
                 <p>powered by <a href="https://vercel.app/">Vercel</a>.</p>
             </div>
         </div>
