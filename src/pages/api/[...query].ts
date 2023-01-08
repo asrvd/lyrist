dd2ae6dcb39dddd429f26ddf176cff779331ab7e
asrvd <asheeshh@duck.com>
2023-01-09 01:26:13 +0530

    refactor: better error handling

--- src/pages/api/[...query].ts
+++ src/pages/api/[...query].ts
@@ -5,21 +5,24 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
-    console.log(req.query);
     const { query } = req.query;
-
     if (query?.length === 2) {
-      const lyrics = await getLyrics(
-        `${decodeURIComponent(query[0] as string)} ${decodeURIComponent(
-          query[1] as string
-        )}`
-      );
-      res.setHeader(
-        "Cache-Control",
-        "public, s-maxage=86400, stale-while-revalidate=43200"
-      );
-      res.setHeader("Content-Type", "application/json");
-      return res.status(200).json(lyrics);
+      try {
+        const lyrics = await getLyrics(
+          `${decodeURIComponent(query[0] as string)} ${decodeURIComponent(
+            query[1] as string
+          )}`
+        );
+
+        res.setHeader(
+          "Cache-Control",
+          "public, s-maxage=86400, stale-while-revalidate=43200"
+        );
+        res.setHeader("Content-Type", "application/json");
+        return res.status(200).json(lyrics);
+      } catch (error) {
+        return res.status(404).json({ error: "Lyrics Not found" });
+      }
     } else {
       return res.status(400).json({ error: "Bad request" });
     }
