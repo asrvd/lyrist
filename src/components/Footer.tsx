798216d4dab20e179d88e2137429e7006e42ecc1
asrvd <asheeshh@duck.com>
2024-03-02 10:48:13 +0530

    feat: add analytics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 export default function Footer() {
   return (
-    <p className="flex gap-2 justify-center items-center absolute bottom-0 w-full p-6 font-sans text-sm text-zinc-300 md:w-2/3 md:p-4 lg:w-1/2 ">
+    <p className="flex gap-2 justify-center items-center w-full p-6 font-sans text-sm text-zinc-300 md:w-2/3 md:p-4 lg:w-1/2 ">
       Made with {`<3`} by{" "}
       <a
         className="text-pink-200"
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,16 +3,18 @@ import Header from "./Header";
 import Meta from "./Meta";
 import Footer from "./Footer";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Meta />
-      <main className="min-w-screen relative flex min-h-screen flex-col items-center gap-6 bg-zinc-900 p-4 font-sans">
+      <main className="min-w-screen flex min-h-screen flex-col items-center gap-6 bg-zinc-900 p-4 font-sans">
         <Header />
         {children}
         <Footer />
       </main>
+      <Analytics />
       <Toaster
         toastOptions={{
           style: {
