5c82bca54e30ab298527c767dff807f0437c9406
asrvd <asheeshh@duck.com>
2024-03-02 10:58:04 +0530

    fix: watch for nullish queries

--- src/pages/api/[...query].ts
+++ src/pages/api/[...query].ts
@@ -1,12 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Client } from "genius-lyrics";
 
+const nullishQueries = ["None", "N/A", "null", "undefined"];
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const client = new Client();
     if (req.method === "GET") {
       const { query } = req.query;
-      if (query && query?.length <= 2 && query?.length !== 0) {
+      if (!Array.isArray(query) || !query) {
+        return res.status(400).json({ error: "Bad request" });
+      }
+      if (
+        query.length <= 2 &&
+        query.length !== 0 &&
+        !query.some((q) => nullishQueries.includes(q))
+      ) {
         try {
           const searches = await client.songs.search(
             `${decodeURIComponent(query[0] as string)} ${decodeURIComponent(
